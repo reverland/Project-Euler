2bab0d61f937f7e4bd6948c7d1d6488e4ed77845
Liu yuyang <lhtlyy@gmail.com>
2012-04-21 21:50:56 +0800

    pro5

--- pro5.lisp
+++ pro5.lisp
@@ -6,3 +6,5 @@
   (apply #'lcm (loop for i from 1 to limit collect i))) 
 (defun euler5 () (full-lcm 20))
 (euler5)
+;then I found a easier one.......
+(reduce #'lcm '(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20))
